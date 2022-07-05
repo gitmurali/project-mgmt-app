4eb1f12e05a0886099cc88b3ddb04e8d301e4516
gitmurali <murali.nutalapatii@gmail.com>
2022-07-05 22:20:36 +0100

    chore: add client row delete

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,18 @@
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="button btn btn-danger">
+        <button className="button btn btn-danger" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
