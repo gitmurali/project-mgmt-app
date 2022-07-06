cf3e36fe403841b246c4f0cacfe47365c8ac31b3
gitmurali <murali.nutalapatii@gmail.com>
2022-07-06 07:58:25 +0100

    add projects and project card

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import Header from "./components/Header";
+import Header from "components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "components/Clients";
 import AddClientModal from "components/AddClientModal";
+import Projects from "components/Projects";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -34,6 +35,7 @@ function App() {
         <Header />
         <div className="Container">
           <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
