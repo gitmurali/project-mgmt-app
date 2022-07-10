a0597d361f1034b7321b992017d1a33fe992236b
gitmurali <murali.nutalapatii@gmail.com>
2022-07-10 20:23:46 +0100

    update project

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "queries/projectQueries";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Spinner from "components/Spinner";
 
 export default function Project() {
@@ -10,6 +10,15 @@ export default function Project() {
   if (loading) return <Spinner />;
   if (error) return <p>something went wrong!</p>;
 
-  console.log(data);
-  return <div>Project</div>;
+  return (
+    <>
+      {!loading & !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
+            Back
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
