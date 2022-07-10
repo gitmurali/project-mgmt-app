27b625c10909ff21babb4a8047fac98255ba17f0
gitmurali <murali.nutalapatii@gmail.com>
2022-07-10 19:48:49 +0100

    chore: add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
 import Header from "components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import Clients from "components/Clients";
-import AddClientModal from "components/AddClientModal";
-import Projects from "components/Projects";
+import Home from "pages/Home";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,9 +39,9 @@ function App() {
         <Router>
           <Header />
           <div className="Container">
-            <AddClientModal />
-            <Projects />
-            <Clients />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
           </div>
         </Router>
       </ApolloProvider>
